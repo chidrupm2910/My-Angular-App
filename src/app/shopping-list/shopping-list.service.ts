9a126e16df7205462af28e093fce7826f6e2bc9f
EC2 Default User <ec2-user@ip-172-31-5-17.us-east-2.compute.internal>
2018-10-19 06:03:02 +0000

    Observables Added - Subject

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,12 @@
 import {Ingredient} from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
+
+import {Subject} from 'rxjs';
 export class ShoppingListService {
      private ingredients : Ingredient[] = [
     new Ingredient('Apple', 5),
     new Ingredient('Tomatoes', 10)]
     
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
     
     getIngredients(){
       return  this.ingredients.slice();
@@ -13,10 +14,10 @@ export class ShoppingListService {
     
     addIngredient(ingredient: Ingredient){
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
     addRecipeIngredirents(ingredients: Ingredient[]){
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
